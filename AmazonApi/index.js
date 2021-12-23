f96cb6518e004492aa9919b9c71cb4daa0b705e7
Adesh Rai <adeshrai@Adeshs-MacBook-Pro-2.local>
2021-12-22 22:04:37 -0500

    getting api key and productId

--- AmazonApi/index.js
+++ AmazonApi/index.js
@@ -4,10 +4,26 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+const apiKey = '';
+const baseUrl = `htto://api.com`
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to Amazon Scrapper API.');
+});
+
+//GET product detail
+app.get('/products/:productId', async(req, res) =>
+{
+    const { productId } = req.params;
+
+    try{
+        const response = await request(`${baseUrl}&url=https//www.amazon.com/dp/${productId}`);
+        res.json(response);
+    }catch(error){
+        res.json(error);
+    }
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
