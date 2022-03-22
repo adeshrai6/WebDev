f178837c5bc9c118bdb531e003169059f90cfd31
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-03-21 22:42:49 -0400

    creating constant variable and submit event listener

---
+++ MealFinder/script.js
@@ -0,0 +1,14 @@
+const search = document.getElementById('search');
+const submit = document.getElementById('submit');
+const random = document.getElementById('random');
+const mealsEl = document.getElementById('meals');
+const resultHeading = document.getElementById('result-heading');
+const single_mealEl = document.getElementById('single-meal');
+
+//Search meal and fetch from API
+function searchMeal(e){
+    e.preventDefault();
+}
+
+//Event listeners
+submit.addEventListener('submit', searchMeal);
