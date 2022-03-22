1127142eccd8c0ff895841afcc890d5edd0e55f8
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-03-22 11:48:28 -0400

    getting single meal with all information and outputting in DOM

--- MealFinder/script.js
+++ MealFinder/script.js
@@ -46,9 +46,42 @@ function searchMeal(e){
 
 //Fetch meal by ID
 function getMealById(mealID){
-    fetch(`www.themealdb.com/api/json/v1/1/lookup.php?i=52772`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0];
+            
+            addMealToDOM(meal);
+        });
 }
 
+function addMealToDOM(meal){
+    const ingredients = [];
+
+    for(let i = 1; i <= 20; i++){
+        if(meal[`strIngredient${i}`]){
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+        }else{
+            break;
+        }
+    }
+
+    single_mealEl.innerHTML = `
+    <div class="single-meal"> 
+        <h1>${meal.strMeal}</h1>
+        <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+        <div class="single-meal-info">
+            ${meal.strCategory ? `<p>${meal.stCategory}</p>` : ''}
+            ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+        </div>
+        <div class="main">
+            <p>${meal.strInstructions}</p>
+            <h2> Ingredients </h2>
+            <ul>${ingredients.map(ing => `<li>${ing}</li>`).join('')}</ul>
+        </div>
+    </div>
+    `;
+}
 
 //Event listeners
 submit.addEventListener('submit', searchMeal);
