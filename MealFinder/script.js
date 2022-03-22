a9aaa8197e629a9401bf129fd0b3b6a4afd0c20d
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-03-22 11:04:28 -0400

    Getting information from DOM when we click them

--- MealFinder/script.js
+++ MealFinder/script.js
@@ -48,3 +48,14 @@ function searchMeal(e){
 
 //Event listeners
 submit.addEventListener('submit', searchMeal);
+
+mealsEl.addEventListener('click', e => {
+    const mealInfo = e.path.find(item => {
+        if(item.classList){
+            return item.classList.contains('meal-info');
+        }else{
+            return false;
+        }
+    });
+
+});
