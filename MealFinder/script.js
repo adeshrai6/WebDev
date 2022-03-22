d938f2a8b4f9e635be508027d8f51a9dba0a2739
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-03-21 22:55:20 -0400

    checking if input is empty and fetching data from api

--- MealFinder/script.js
+++ MealFinder/script.js
@@ -8,7 +8,27 @@ const single_mealEl = document.getElementById('single-meal');
 //Search meal and fetch from API
 function searchMeal(e){
     e.preventDefault();
+
+    //Clear single meal
+    single_mealEl.innerHTML = '';
+
+    //Get search term
+    const term = search.value;
+
+    // Check for empty
+    if(term.trim()){
+        fetch(`www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            });
+    }else{
+        alert('Please enter somthing in the input area');
+    }
+
 }
+
+
 
 //Event listeners
 submit.addEventListener('submit', searchMeal);
