c93d4db3b9a27777ee6a94f689f74593b13ee00e
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-04-01 23:39:25 -0400

    getting HTML element into DOM

---
+++ MusicPlayer/script.js
@@ -0,0 +1,10 @@
+const musicContainer = document.getElementById('music_container');
+const playBth = document.getElementById('play');
+const prevBth = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
