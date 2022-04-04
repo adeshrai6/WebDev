6c089e05fd92829ed51592e7298a5d102232abd4
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-04-03 23:44:45 -0400

    clean

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -1,45 +1,46 @@
-const musicContainer = document.getElementById('music-container');
-const playBth = document.getElementById('play');
-const prevBth = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
+const musicContainer = document.getElementById("music-container");
+const playBth = document.getElementById("play");
+const prevBth = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
 
-const audio = document.getElementById('audio');
-const progress = document.getElementById('progress');
-const progressContainer = document.getElementById('progress-container');
-const title = document.getElementById('title');
-const cover = document.getElementById('cover');
+const audio = document.getElementById("audio");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
+const title = document.getElementById("title");
+const cover = document.getElementById("cover");
 
 //Song Title
-const songs = ['MutuBhari', 'PahiloJunima', 'parilima'];
+const songs = ["MutuBhari", "PahiloJunima", "parelima"];
 
 // Keep track of song
-let sondIndex = 0;
+let sondIndex = 1;
 
 // Initially load song details into Dom
 
 loadSong(songs[sondIndex]);
 
-function loadSong (song) {
-    title.innerText = song;
-    audio.src = `music/${song}.mp3`;
-    cover.src = `images/${song}.jpeg`;
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpeg`;
 }
 
 //Play Song
-function playSong(){
-    musicContainer.classList.add('play');
-    playBth.querySelector('i.fas').classList.remove('fa-play');
-    playBth.querySelector('i.fas').classList.add('fa-pause');
+function playSong() {
+  musicContainer.classList.add('play');
+  playBth.querySelector('i.fas').classList.remove('fa-play');
+  playBth.querySelector('i.fas').classList.add('fa-pause');
 
-    audio.play();
+  audio.play();
 }
-
 
 // Event Listners
 playBth.addEventListener('click', () => {
-    const isPlaying = musicContainer.classList.contains('play');
+  const isPlaying = musicContainer.classList.contains('play');
 
-    if(isPlaying){pauseSong();}
-    else{playSong();}
-
-})
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
