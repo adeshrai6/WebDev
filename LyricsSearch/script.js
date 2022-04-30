779b964e75747ce0e861b343e0358b517a1db662
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-04-29 23:17:51 -0400

    get data from api and output to DOM

--- LyricsSearch/script.js
+++ LyricsSearch/script.js
@@ -6,3 +6,62 @@ const more = document.getElementById('more');
 
 const apiURL = 'https://api.lyrics.ovh';
 
+
+// Search by song or artist 
+async function searchSongs(term) {
+    // using fetch
+    // fetch(`${apiURL}/suggest/${term}`).then(res => res.json()).then(data => console.log(data));
+
+
+    // async
+    const res = await fetch(`${apiURL}/suggest/${term}`);
+    const data = await res.json();
+
+    showData(data);
+}
+
+
+// Show song and artist in DOM
+function showData(data) {
+    // let output = '';
+
+    // data.data.forEach(song => {
+    //     output += `
+    //         <li>
+    //             <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+    //             <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+    //         </li>
+    //     `;
+    // }); 
+
+    // result.innerHTML = `
+    //     <ul class="songs">
+    //         ${output}
+    //     </ul>
+    // `;
+
+    result.innerHTML = `
+        <ul class="songs">
+            ${data.data.map(song => `
+            <li>
+                <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+                <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+            </li>`).join('')}
+        </ul>
+    `;
+
+    
+}
+
+// Event listeners
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const searchTerm = search.value.trim();
+
+    if(!searchTerm) {
+        alert('Please type in a search term');
+    }else {
+        searchSongs(searchTerm);
+    }
+})
