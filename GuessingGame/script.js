6f337bdcfc6e8145978197ca7aa88099850bc3d0
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-05-03 13:51:54 -0400

    output message to DOM

--- GuessingGame/script.js
+++ GuessingGame/script.js
@@ -13,6 +13,28 @@ let recognition = new window.SpeechRecognition();
 recognition.start();
 
 
+// Capture user speak
+function onSpeak(e) {
+    const msg = e.results[0][0].transcript;
+
+    writeMessage(msg);
+}
+
+
+// Write what user speaks
+function writeMessage(msg) {
+    msgEl.innerHTML = `<div> Your said: </div>
+                        <span class="box">${msg}</span>
+                        `;
+}
+
+
+
+// Generate random number
 function getRandomNum() {
     return Math.floor(Math.random() * 100) + 1;
 }
+
+
+// Speack result
+recognition.addEventListener('result', onSpeak);
