c61518acaeac992306af1c396c9590bd4d7c678f
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-06-07 22:49:03 -0400

    setup

--- RelaxingApp/script.js
+++ RelaxingApp/script.js
@@ -1,6 +1,5 @@
-const container = document.getElementById('container');
-const text = document.getElementById('text');
-
+const container = document.getElementById("container");
+const text = document.getElementById("text");
 
 const totalTime = 7500;
 const breatheTime = (totalTime / 5) * 2;
@@ -9,16 +8,16 @@ const holdTime = totalTime / 5;
 breathAnimation();
 
 function breathAnimation() {
-    text.innerText = 'Breathe In!';
-    container.className = 'container grow';
+  text.innerText = "Breathe In!";
+  container.className = "container grow";
 
+  setTimeout(() => {
+    text.innerText = "Hold";
+    container.className = "container shrink";
     setTimeout(() => {
-        text.innerText = 'Hold';
-        container.className = 'container shrink'
-        setTimeout(() => {
-            text.innerText = 'Breathe Out!';
-        }, holdTime);
-    }, breatheTime);
+      text.innerText = "Breathe Out!";
+    }, holdTime);
+  }, breatheTime);
 }
 
 setInterval(breathAnimation, totalTime);
