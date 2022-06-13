2f83032196f9334871df9a177c7d5a9098bbb035
Adesh Rai <61568773+adeshrai6@users.noreply.github.com>
2022-06-12 23:38:09 -0400

    getting api with unique id

--- NewsLetter/app.js
+++ NewsLetter/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const e = require("express");
+const https = require("https");
 
 const app = express();
 
@@ -17,9 +18,35 @@ app.post("/", function(req, res) {
     let firstName = req.body.fName;
     let lastName = req.body.lName;
     let email = req.body.email;
-    console.log(firstName, lastName, email)
-})
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subcribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data); 
+
+    const url = "https://us18.api.mailchimp.com/3.0/lists/b83768305c"
+
+    https.request(url, options, function(response) {
+
+    })
+});
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000");
 });
+
+// Api Key
+f26de0052b3b99fe7e9d0eb428e4e7c6-us18
+
+// List key
+b83768305c
